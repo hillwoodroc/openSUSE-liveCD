8cff100a3ce3883f48752144bb985d3f4d6e82dd
hillwoodroc <hillwoodroc@gmail.com>
2016-09-16 20:43:35 +0800

    Fix bug.

--- livecd-gnome/root/var/lib/livecd/geturls.sh
+++ livecd-gnome/root/var/lib/livecd/geturls.sh
@@ -2,5 +2,5 @@ zypper addrepo -f  -n "openSUSE-42.1-Update" https://mirrors.ustc.edu.cn/opensus
 zypper addrepo -f  -n "openSUSE-42.1-Update-Non-Oss" https://mirrors.ustc.edu.cn/opensuse/update/leap/42.1/non-oss/ repo-update-non-oss
 zypper addrepo -f  -n "openSUSE-42.1-Oss" https://mirrors.ustc.edu.cn/opensuse/distribution/leap/42.1/repo/oss/ repo-oss
 zypper addrepo -f  -n "openSUSE-42.1-Non-Oss" https://mirrors.ustc.edu.cn/opensuse/distribution/leap/42.1/repo/non-oss/ repo-non-oss
-zypper addrepo -f  -n "Packman Repository" http://ftp.halifax.rwth-aachen.de/packman/suse/openSUSE_Leap_42.1/ repo-packman
+zypper addrepo -f  -n "Packman Repository" http://ftp.twaren.net/Linux/Packman/suse/openSUSE_Leap_42.1/ repo-packman
 zypper addrepo -f  -n "openSUSE for Chinese Users Project" http://download.opensuse.org/repositories/home:/opensuse_zh/openSUSE_Leap_42.1/ repo-home-opensuse-zh
